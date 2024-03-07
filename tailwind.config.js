c8afe3cd7d885d9c33bbc09b20947672159e414a
KK <k.koichi276052@gmail.com>
2024-03-07 18:18:06 +0900

    add preline add taos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ""),
+    files: ["./src/*.{html,js}"],
+  },
   content: [
     "./public/**/*.{js,css}",
     "./views/**/*.ejs",
     "./node_modules/tw-elements/dist/js/**/*.js",
+    "node_modules/preline/dist/*.js",
   ],
   darkMode: "class",
-  plugins: [require("tw-elements/dist/plugin.cjs")],
+  plugins: [
+    require("tw-elements/dist/plugin.cjs"),
+    require("taos/plugin"),
+    require("preline/plugin"),
+  ],
+  safelist: [
+    "!duration-[0ms]",
+    "!delay-[0ms]",
+    'html.js :where([class*="taos:"]:not(.taos-init))',
+  ],
 };
